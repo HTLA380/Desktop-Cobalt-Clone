45229f20c3a3f5ba8baa05f705fe896e6b67df50
Htet Aung Lin <htetlin099798@gmail.com>
2024-05-08 20:05:03 +0630

    feat: add navbar

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+interface ButtonInterface {
+  content: string;
+}
+
+const Button: React.FC<ButtonInterface> = ({ content }) => {
+  return (
+    <button
+      className='relative p-px duration-300 rounded-full group text-sm/6 text-zinc-400 hover:text-zinc-100 shadow-cyan-glow'
+      style={{ fontWeight: 'inherit' }}
+      type='button'>
+      <span className='absolute inset-0 overflow-hidden rounded-full'>
+        <span className='absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100'></span>
+      </span>
+      <div className='relative z-10 rounded-full bg-zinc-950 px-4 py-1.5 ring-1 ring-white/10'>{content}</div>
+      <span className='absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-cyan-400/0 via-cyan-400/90 to-cyan-400/0 transition-opacity duration-500 group-hover:opacity-40'></span>
+    </button>
+  );
+};
+
+export default Button;
