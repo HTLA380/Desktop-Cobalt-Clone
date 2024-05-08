5be89d954953ed362d3c8be7b6a1f326773470ac
Htet Aung Lin <htetlin099798@gmail.com>
2024-05-08 20:35:50 +0630

    feat: add hero section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
+import './css/globals.css';
 import Navbar from '@/components/navbar/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import HeroSection from '@/containers/hero-section';
 import React from 'react';
 
 const Home = () => {
-  return <div>this is a Home page</div>;
+  return (
+    <div>
+      <HeroSection />
+    </div>
+  );
 };
 
 export default Home;
