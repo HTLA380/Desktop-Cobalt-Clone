96191b61fedebb88d4b25d07f4ff379ecfac5a2d
Htet Aung Lin <htetlin099798@gmail.com>
2024-05-09 09:56:01 +0630

    feat: add section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from '@/containers/hero-section';
+import IntroSection from '@/containers/intro-section';
 import React from 'react';
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
+      <IntroSection />
     </div>
   );
 };
---
+++ src/components/text/GradientText.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+interface GradientTextInterface {
+  children: string;
+  Wrapper?: React.ElementType;
+  className?: string;
+}
+
+const GradientText: React.FC<GradientTextInterface> = ({ children, Wrapper = 'h3', className }) => {
+  return (
+    <Wrapper
+      className={`text-[1.9rem]/[1.2] font-extrabold tracking-tight md:text-5xl/[1.2] bg-gradient-to-br from-white to-zinc-500 bg-clip-text text-transparent ${className}`}>
+      {children}
+    </Wrapper>
+  );
+};
+
+export default GradientText;
